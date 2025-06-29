9ccb90274c5ed5d1dda3441392cb31ceabb35370
Ariel Carmen <arielahognisse@gmail.com>
2025-06-29 21:32:23 +0100

    feat: locales initialization

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,28 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'fr_FR': Locales.fr_FR,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const welcome = 'welcome';
+  static const vaccine = 'vaccine';
+  static const my_vaccines = 'my_vaccines';
+  static const scan = 'scan';
+  static const generate = 'generate';
+}
+
+class Locales {
+  static const fr_FR = {
+    'welcome': 'Bienvenue',
+    'vaccine': 'Vaccin',
+    'my_vaccines': 'Mes vaccin',
+    'scan': 'Scanner',
+    'generate': 'Génerer Qr',
+  };
+}
